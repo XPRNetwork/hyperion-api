8c911f9209f785d6ca556371a66f7b09ed428626
Denis Carriere <carriere.denis@gmail.com>
2019-03-08 10:03:13 -0500

    Fix types

---
+++ src/types/get_abi_snapshot.d.ts
@@ -0,0 +1,45 @@
+export interface GetAbiSnapshotResponse {
+  version: string;
+  types: Type[];
+  structs: Struct[];
+  actions: Action[];
+  tables: Table[];
+  ricardian_clauses: Ricardianclause[];
+  error_messages: any[];
+  abi_extensions: any[];
+}
+
+interface Ricardianclause {
+  id: string;
+  body: string;
+}
+
+interface Table {
+  name: string;
+  index_type: string;
+  key_names: string[];
+  key_types: string[];
+  type: string;
+}
+
+interface Action {
+  name: string;
+  type: string;
+  ricardian_contract: string;
+}
+
+interface Struct {
+  name: string;
+  base: string;
+  fields: Field[];
+}
+
+interface Field {
+  name: string;
+  type: string;
+}
+
+interface Type {
+  new_type_name: string;
+  type: string;
+}
